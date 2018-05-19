901e4c083c26bc1cc5f321b4b944866fc2351f39
William Li <githuberr@outlook.com>
2018-05-19 00:26:28 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,14 +1,27 @@
-def take_a_number(current_line, new_person)
-  current_line << new_person
-  current_line.length
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
-def now_serving(current_line)
-  puts "Currently serving #{current_line.first}"
-  current_line.shift
+def now_serving(katz_deli)
+  if katz_deli.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli.shift}."
+  end
+
 end
 
-def line(current_line)
-  puts "The line is currently: "
-  current_line.each_with_index{|x,i| puts "#{i+1}. #{x} "}
+def line(katz_deli)
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else
+    message="The line is currently:"
+
+    katz_deli.each_with_index do |value, index|
+      message += " #{index.to_i+1}. #{value}"
+    end
+
+    puts "#{message}"
+  end
 end
