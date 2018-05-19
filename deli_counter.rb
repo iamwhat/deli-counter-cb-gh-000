946fb287ef4dac0f48d23dfb5706609dbcb91e80
William Li <githuberr@outlook.com>
2018-05-19 00:25:50 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,14 @@
-# Write your code here.
+def take_a_number(current_line, new_person)
+  current_line << new_person
+  current_line.length
+end
 
-def line(arr)
-  
+def now_serving(current_line)
+  puts "Currently serving #{current_line.first}"
+  current_line.shift
+end
 
-def take_a_number(line, person)
+def line(current_line)
+  puts "The line is currently: "
+  current_line.each_with_index{|x,i| puts "#{i+1}. #{x} "}
+end
